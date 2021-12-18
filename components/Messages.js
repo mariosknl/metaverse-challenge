70eb017eaf401e7701720da78add200eab685904
mariosknl <mariosknl@outlook.com>
2021-12-18 18:28:56 +0200

    implementing timeago-react to show timestamps next to the messages

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,28 @@
 import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
+
+// Only show messages from the last 15 minutes
+const MINS_DURATION = 15;
 
 function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
 
   return (
     <div className="pb-56">
@@ -15,7 +33,11 @@ function Messages() {
         />
       </div>
 
-      <div>{/* Each Message */}</div>
+      <div className="space-y-10 p-4">
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
 
       <div className="flex justify-center">
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
